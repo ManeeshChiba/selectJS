230b707e693bf02445e1f32fc688faa011db980a
Maneesh <maneesh@rfc.private.address.invalid.query>
2015-04-18 22:49:16 +0200

    Began scraping data from existing select elements.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 $(function() {
+
+	if ( $('select').length > 0 ){
+
+		$('select').each(function(selectIndex){
+			$(this).addClass('selectReplace'+selectIndex);
+			var selectName = $(this).attr('name');
+			if ( $(this).attr('id') != undefined && $(this).attr('id') != false ){
+				var selectId = $(this).attr('id');
+			}
+			var arrOptionName = new Array();
+			var arrOptionValue = new Array();
+
+			$('option', this).each(function(optionIndex){
+				arrOptionValue[optionIndex] = $(this).attr('value');
+				arrOptionName[optionIndex] = $(this).text();
+			});
+		});
+
+	}
    
 	$('.faux-select').click(function(){
 	  $(this).toggleClass('open');
